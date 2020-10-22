f0d29ae37d27975d74b2938639b5d1cf1d695394
Jhorkman Bernal <jhorber95@gmail.com>
2020-10-21 19:48:26 -0500

    add Atributos list

--- src/main/java/com/soft/data/service/ToyDataService.java
+++ src/main/java/com/soft/data/service/ToyDataService.java
@@ -1,5 +1,6 @@
 package com.soft.data.service;
 
+import com.soft.data.domain.AtributoUnidadControlDto;
 import com.soft.data.domain.CatalogoDto;
 import com.soft.data.domain.CuentaDto;
 import com.soft.data.domain.UnidadControlDto;
@@ -7,6 +8,7 @@ import com.soft.data.util.Constants;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -21,7 +23,7 @@ public class ToyDataService {
         c.setDesCuenta("Cuenta de Saldos Deudores TI");
         c.setCodEstadoCuenta("1");
         c.setCodTipoCuenta(createCatalogo("CD10"));
-        c.setUnidadControl(createUnidad("1", Constants.DES_UNI_CONT_1));
+        c.setUnidadControl(createUnidad("1"));
 
         CuentaDto c1 = new CuentaDto();
         c1.setCodCuenta("CD1021");
@@ -29,8 +31,7 @@ public class ToyDataService {
         c1.setDesCuenta("Cuentade Sanciones TI");
         c1.setCodEstadoCuenta("1");
         c1.setCodTipoCuenta(createCatalogo("CD11"));
-        c1.setUnidadControl(createUnidad("1", Constants.DES_UNI_CONT_1));
-
+        c1.setUnidadControl(createUnidad("1"));
 
 
         CuentaDto c2 = new CuentaDto();
@@ -39,8 +40,7 @@ public class ToyDataService {
         c2.setDesCuenta("Cuenta de  Valores TI (OP,RM,RD,RIP)");
         c2.setCodEstadoCuenta("1");
         c2.setCodTipoCuenta(createCatalogo("CD12"));
-        c2.setUnidadControl(createUnidad("1", Constants.DES_UNI_CONT_1));
-
+        c2.setUnidadControl(createUnidad("1"));
 
 
         CuentaDto c3 = new CuentaDto();
@@ -49,8 +49,7 @@ public class ToyDataService {
         c3.setDesCuenta("Cuenta de  Valores TA (OP, RM, RD, RSIDV, RIP)");
         c3.setCodEstadoCuenta("1");
         c3.setCodTipoCuenta(createCatalogo("CD13"));
-        c3.setUnidadControl(createUnidad("1", Constants.DES_UNI_CONT_1));
-
+        c3.setUnidadControl(createUnidad("1"));
 
 
         CuentaDto c4 = new CuentaDto();
@@ -59,7 +58,7 @@ public class ToyDataService {
         c4.setDesCuenta("Cuenta de  Fraccionamiento");
         c4.setCodEstadoCuenta("1");
         c4.setCodTipoCuenta(createCatalogo("CD14"));
-        c4.setUnidadControl(createUnidad("1", Constants.DES_UNI_CONT_1));
+        c4.setUnidadControl(createUnidad("1"));
 
 
         List<CuentaDto> cuentaDtos = new ArrayList<>();
@@ -76,16 +75,71 @@ public class ToyDataService {
     }
 
     private CatalogoDto createCatalogo(String cod) {
-       return new CatalogoDto(cod, cod);
+        return new CatalogoDto(cod, cod);
     }
 
-    private UnidadControlDto createUnidad(String cod, String des ) {
+    private UnidadControlDto createUnidad(String cod) {
         UnidadControlDto dto = new UnidadControlDto();
         dto.setCodUnidadControl(cod);
-        dto.setDesUnidadControl("RUC/".concat(des));
+        switch (cod) {
+            case "1":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_1));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getCodTributo("2"), getPerDocumento("3")));
+                break;
+            case "2":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_2));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getCodTributo("2"), getPerDocumento("3"), getNumValor("4")));
+                break;
+            case "3":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_3));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getCodTributo("2")));
+                break;
+            case "4":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_4));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getCodTributo("2"), getEjercicio("3")));
+                break;
+            case "5":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_5));
+                break;
+            case "6":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_6));
+                break;
+            case "7":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_7));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getCodTributo("2"), getEjercicio("3")));
+                break;
+            case "8":
+                dto.setDesUnidadControl("RUC/".concat(Constants.DES_UNI_CONT_8));
+                dto.setLisAtributosUnidadControl(Arrays.asList(getNumRuc("1"), getEjercicio("2")));
+                break;
+
+        }
+
+
         dto.setCodEstado("1");
         dto.setIndEstadoRegistro("1");
         dto.setNumLoadProcessEvent("99991231");
         return dto;
     }
+
+    private AtributoUnidadControlDto getNumRuc(String orden) {
+        return new AtributoUnidadControlDto("numRuc", "RUC", "", "str", orden);
+    }
+
+    private AtributoUnidadControlDto getCodTributo(String orden) {
+        return new AtributoUnidadControlDto("codTributo", "codTributo", "", "str", orden);
+    }
+
+    private AtributoUnidadControlDto getPerDocumento(String orden) {
+        return new AtributoUnidadControlDto("perDocumento", "perDocumento", "", "str", orden);
+    }
+
+    private AtributoUnidadControlDto getNumValor(String orden) {
+        return new AtributoUnidadControlDto("numValor", "numValor", "", "str", orden);
+    }
+
+    private AtributoUnidadControlDto getEjercicio(String orden) {
+        return new AtributoUnidadControlDto("ejercicio", "ejercicio", "", "str", orden);
+    }
+
 }
